ff9655c8409b9d6db234e0c77a9a28fc034b0f60
Vithushan <sutharshanvithushan@gmail.com>
2024-11-16 03:15:46 +0530

    docs: reverse example

--- example.go
+++ example.go
@@ -3,23 +3,17 @@ package main
 import "fmt"
 
 func simulateExampleWorkflow() {
-	result_for_viewer := viewerRequestController("vieweraddress0000", "controlleraddress0000")
-	fmt.Printf("Result from viewRequestController 0: %d\n", result_for_viewer)
-
-	fmt.Println(pendingControllers)
-	fmt.Println(pendingViewers)
 	result_for_controller := controllerRequestViewer("controlleraddress0000", "vieweraddress0000")
-	fmt.Printf("Result from viewRequestController 0: %d\n", result_for_controller)
+	fmt.Printf("Result from controllerRequestViewer 0: %d\n", result_for_controller)
 
-	fmt.Println(pendingControllers)
-	fmt.Println(pendingViewers)
-	result_for_new_controller := controllerRequestViewer("controlleraddress1111", "vieweraddress1111")
-	fmt.Printf("Result from viewRequestController 1: %d\n", result_for_new_controller)
+	result_for_viewer := viewerRequestController("vieweraddress0000", "controlleraddress0000")
+	fmt.Printf("Result from viewerRequestController 0: %d\n", result_for_viewer)
 
-	fmt.Println(pendingControllers)
-	fmt.Println(pendingViewers)
 	result_for_new_viewer := viewerRequestController("vieweraddress1111", "controlleraddress1111")
-	fmt.Printf("Result from viewRequestController 1: %d\n", result_for_new_viewer)
+	fmt.Printf("Result from viewerRequestController 1: %d\n", result_for_new_viewer)
+
+	result_for_new_controller := controllerRequestViewer("controlleraddress1111", "vieweraddress1111")
+	fmt.Printf("Result from controllerRequestViewer 1: %d\n", result_for_new_controller)
 
 	fmt.Println(pendingControllers)
 	fmt.Println(pendingViewers)
